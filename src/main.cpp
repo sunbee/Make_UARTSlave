2f0ab6a29307159e6146cb12b20aa91dec7aa793
sunbee <sanjay.bhatikar@gmail.com>
2021-08-01 14:39:16 -0500

    First payload

---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+
+#include <Wire.h>
+#include <SerialTransfer.h>
+#include <SoftwareSerial.h>
+
+SerialTransfer slaveMCU;
+SoftwareSerial Extra(2, 3); // Rx: 2, Tx: 3
+unsigned long tic = millis();
+unsigned long toc = tic;
+
+struct PAYMASTER {
+  /*
+  water: instruction to switch pump on or off. Note the float sensor in pump's circuit will prevent overflow.
+  fan: instruction to control fan speed - LO, MED, HIGH. Note PC fan requires an int between 0 and 255.
+  led: instruction to control LED brightness. Note that the FastLED library requires an int between 0 and 255.
+  */
+  bool water;
+  uint8_t fan; 
+  uint8_t led;
+} instructions;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(201);
+  Extra.begin(9600);
+  delay(201);
+  slaveMCU.begin(Extra);
+  delay(201);
+}
+
+void debug() {
+  Serial.print("SLAVE: ");
+  Serial.print(millis());
+  Serial.print("   Water: ");
+  Serial.print((bool)instructions.water);
+  Serial.print(", Fan: ");
+  Serial.print(instructions.fan);
+  Serial.print(", LED: ");
+  Serial.println(instructions.led);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (slaveMCU.available()) {
+    slaveMCU.rxObj(instructions);
+    debug();
+  } else if (slaveMCU.status < 0) {
+    Serial.print("ERROR: ");
+
+    if(slaveMCU.status == -1)
+      Serial.println(F("CRC_ERROR"));
+    else if(slaveMCU.status == -2)
+      Serial.println(F("PAYLOAD_ERROR"));
+    else if(slaveMCU.status == -3)
+      Serial.println(F("STOP_BYTE_ERROR"));
+  }
+}
